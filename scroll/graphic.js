35402314c8939ac1d59a246ecd15c4fe83018f42
Lucia Blanco Llera <56401624+blluci@users.noreply.github.com>
2021-04-09 09:20:33 +0200

    Changes to css

--- scroll/graphic.js
+++ scroll/graphic.js
@@ -13,9 +13,16 @@ window.createGraphic = function() {
     var graphicEl = d3.select('.graphic')
     var graphicVisEl = graphicEl.select('.graphic__vis')
     var graphicProseEl = graphicEl.select('.graphic__prose')
+    
+    
 
     var size = 600
     var height = 600
+    
+    var svg = graphicVisEl.append('svg')
+            .attr('width', size + 'px')
+            .attr('height', height + 'px')
+    
 
     // actions to take on each step of our scroll-driven story
     var steps = [
@@ -25,6 +32,24 @@ window.createGraphic = function() {
 
         function step1() {
             console.log("step1");
+            var imgs = svg.selectAll("img").data([0]);
+            imgs.enter()
+                .append("img")
+                .attr("xlink:href", "snake.png")
+                .attr("x", "60")
+                .attr("y", "60")
+                .attr("width", "20")
+                .attr("height", "20");
+            
+            
+            
+            svg
+            .append("svg:image")
+            .attr("xlink:href", "snake.png")
+            .attr("width", 600)
+            .attr("height", 600)
+            .attr("x", 0)
+            .attr("y", 0);
         },
 
         function step2() {
@@ -49,15 +74,10 @@ window.createGraphic = function() {
     }
 
     function setupCharts() {
-        var svg = graphicVisEl.append('svg')
-            .attr('width', size + 'px')
-            .attr('height', height + 'px')
-        
         svg.append("rect")
             .attr("width", "100%")
             .attr("height", "100%")
             .attr("fill", "#36515B");
-
     }
 
     function setupProse() {
