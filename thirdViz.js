a93e90fe1301d84acde2233c9a76d8416a3725d6
Andrea López Suárez <uo264703@uniovi.es>
2021-04-29 12:43:26 +0200

    Add introduction and finish tooltip

--- thirdViz.js
+++ thirdViz.js
@@ -1,35 +1,38 @@
 //<-----------------------------------------THIRD VIZ--------------------------------------------------------->
 // VARIABLES ****************************************************
-const width = 1450,
-    height = 900;
+const width = 750,
+    height = 700;
 let svgSolid, solid_group;
+let tooltip;
 
-let imagesAttributes = [{ name: "privacy", type: "svg", pos: "tooltip-right" },
-    { name: "profile", type: "svg", pos: "tooltip-right" },
-    { name: "database-storage", type: "svg", pos: "tooltip-right" },
-    { name: "inrupt", type: "png", pos: "tooltip-left" },
-    { name: "empathy-imagotype-white", type: "png", pos: "tooltip-left" },
-    { name: "tim-berners-lee", type: "png", pos: "tooltip-left" }
+let imagesAttributes = [{ name: "privacy", type: "svg" },
+    { name: "profile", type: "svg" },
+    { name: "database-storage", type: "svg" },
+    { name: "inrupt", type: "png" },
+    { name: "empathy-imagotype-white", type: "png" },
+    { name: "tim-berners-lee", type: "png" }
 ];
 
 let imagesText = {
-    "privacy": "Users control their data and decide to whom they share it",
-    "profile": "Apps can access to user information from any Pod, only with user permission",
-    "database-storage": "Pods store user data in an interoperable format and provide users with control permissions",
-    "inrupt": "There are some Pod Providers where you can create your Pod, such as Inrupt.  You could also self-host your own Pod running a Pod Server",
-    "empathy-imagotype-white": "Empathy believes in this specification, as the company is developing some projects like <a href='https://ohmypod.netlify.app'>OhMyPod</a> or <a href='https://kala.empathy.co/'>Kala.</a>",
-    "tim-berners-lee": "SOLID is a project led by Tim Berners-Lee, the World Wide Web inventor.  It realizes his original vision of the Web."
+    "privacy": "Users <span class='highlight'>control</span> their data and decide to <span class='highlight'>whom</span> they share it",
+    "profile": "<span class='highlight'>Apps</span> can access to user information from any Pod, only with  <span class='highlight'>user permission</span>",
+    "database-storage": "Pods <span class='highlight'>store</span> user data in an <span class='highlight'>interoperable format</span> and provide users with control permissions",
+    "inrupt": "There are some <span class='highlight'>Pod Providers</span> where you can create your Pod, such as <a href='https://inrupt.com/solid/' target='blank'>Inrupt</a>.  You could also self-host your own Pod running a Pod Server",
+    "empathy-imagotype-white": "<span class='highlight'>Empathy</span> believes in this specification, as the company is developing some projects like <a href='https://ohmypod.netlify.app' target='blank'>OhMyPod</a> or <a target='blank' href='https://kala.empathy.co/'>Kala.</a>",
+    "tim-berners-lee": "SOLID is a project led by <span class='highlight'>Tim Berners-Lee</span>, the World Wide Web inventor.  It realizes his <span class='highlight'>original vision</span> of the Web."
 }
 
 // SHOW THIRD VIZ ***********************************************
 window.addEventListener("load", function() {
-    svgSolid = d3.select("div.third_viz")
+    svgSolid = d3.select("#SOLID-content")
         .append("svg")
         .attr("preserveAspectRatio", "xMidYMid meet")
         .attr("viewBox", `0 0 ${width} ${height}`);
 
     solid_group = svgSolid.append("g").attr("id", "group-SOLID")
         .attr("transform", "translate(0, 23)");
+
+    tooltip = d3.select("#SOLID_tooltip_wrapper");
 
     createGraph();
 }, false);
@@ -89,24 +92,28 @@ function createIcons() {
     imagesAttributes.forEach(image => {
         groupIcons.append("image").attr("id", `svg-SOLID-${image.name}-icon`)
             .attr("xlink:href", base_path_image + `${image.name}.${image.type}`)
-            .on("mouseover", () => {
-                showTooltip(d3.event.pageX, d3.event.pageY, image.pos, imagesText[image.name], image.name);
-            });
+            .on("mouseover", () => showTooltip(d3.event.pageX, d3.event.pageY, image.name));
     })
 }
 
-function showTooltip(x, y, pos, text, name) {
-    let tooltip = d3.select("#SOLID_tooltip_wrapper");
-
+function showTooltip(x, y, imageName) {
     tooltip.transition()
-        .duration(500)
-        .style("opacity", "0");
-    tooltip.transition()
-        .duration(200)
+        .duration(100)
         .style("opacity", "1");
 
-    tooltip.select("#SOLID_tooltip").attr("class", pos).html(text);
     tooltip.style("left", (x) + "px")
         .style("top", (y) + "px");
+
+    tooltip.select("#SOLID_tooltip").html(imagesText[imageName]);
+
+}
+
+function hideTooltip() {
+    tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+}
+
+function showInfo(image) {
 
 }
